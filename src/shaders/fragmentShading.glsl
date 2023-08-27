4b451daed51ed947c1b3567964295aac982b8dbc
developmentHF <development.henry@gmail.com>
2023-08-27 20:43:35 +1000

    flip direction of eyes

--- src/shaders/fragmentShading.glsl
+++ src/shaders/fragmentShading.glsl
@@ -5,10 +5,12 @@ out vec4 FragColor;
 in vec3 ourColor;
 in vec2 TexCoord;
 
+vec2 newCoord = vec2(-TexCoord.x, TexCoord.y);
+
 uniform sampler2D ourTexture;
 uniform sampler2D ourTexture2;
 
 void main()
 {
-    FragColor = mix(texture(ourTexture, TexCoord), texture(ourTexture2, TexCoord), 0.2);
+    FragColor = mix(texture(ourTexture, TexCoord), texture(ourTexture2, newCoord), 0.2);
 }
